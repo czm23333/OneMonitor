bb4adc36f3513d9bc4ff094d62ea53b149ffe154
czm2333 <34388355+czm23333@users.noreply.github.com>
2023-01-22 23:39:30 +0800

    Make id atomic for multi-thread situation

--- src/main/java/io/github/czm23333/onemonitor/minecraft/utils/GraphUtil.java
+++ src/main/java/io/github/czm23333/onemonitor/minecraft/utils/GraphUtil.java
@@ -3,14 +3,16 @@ package io.github.czm23333.onemonitor.minecraft.utils;
 import guru.nidi.graphviz.model.Factory;
 import guru.nidi.graphviz.model.Node;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class GraphUtil {
-    private static long id = 0;
+    private static final AtomicLong id = new AtomicLong(0);
 
     public static void reset() {
-        id = 0;
+        id.set(0);
     }
 
     public static Node newNode(String label) {
-        return Factory.node(String.valueOf(id++)).with("label", label);
+        return Factory.node(String.valueOf(id.getAndIncrement())).with("label", label);
     }
 }
